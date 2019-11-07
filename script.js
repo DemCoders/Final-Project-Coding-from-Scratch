0db7e270520287d19bd1858078c9e1351d1aa3f2
Demetrios <d@barbalios.com>
2019-11-07 11:20:49 -0500

    Final Project Submitted

--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 function idCard() {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
-    var age = document.getElementById('age').value;
+    var age = parseInt(document.getElementById('age').value);
     var address = document.getElementById('address').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
+    var phoneNumber = parseInt(document.getElementById('phoneNumber').value);
     document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
-    document.getElementById("postAge").innerHTML = "Age: " + age;
-    document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
     document.getElementById("postAddress").innerHTML = address;
+
+    var numberArray = [age, phoneNumber];
+    for (var i = 0; i < numberArray.length; i++) {
+        if (numberArray[i] <= 100) {
+            document.getElementById("postAge").innerHTML = "Age: " + age;
+        }
+        else if (numberArray[i] >= 100) {
+            document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+        }
+    }
+    
+
 }
